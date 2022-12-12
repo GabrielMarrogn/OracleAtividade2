4d4c451bcbbe9bcc20124edf03234758d91bc7ba
GabrielMarrogn <gabrielosantos2012@hotmail.com>
2022-12-11 23:36:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Use oito instruções de impressão para recriar a carinha feliz acima.  Sua arte baseia-se em um único caractere, além do espaço, como X ou #. 
+
---
+++ src/Parte2.java
@@ -0,0 +1,15 @@
+public class Parte2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        // it's just a plate with an x on it.
+        System.out.println("  OOOO ");
+        System.out.println(" O    O ");
+        System.out.println("O O  O O");
+        System.out.println("O O  O O");
+        System.out.println("O  OO  O");
+        System.out.println("O O  O O");
+        System.out.println(" O    O ");
+        System.out.println("  OOOO  "); 
+}
+}
